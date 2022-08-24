6efeb7223d00e312fdf53b986b92cbef90837efe
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2022-08-25 01:36:12 +0300

    fix: eng

--- src/views/WorkInProgress.vue
+++ src/views/WorkInProgress.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col items-center container h-screen pt-12">
     <p class="text-subtitle md:text-title text-center">🚧 Work in Progress 🚧</p>
     <p class="text-body md:text-body-big text-center mt-6">
-      In the meanwhile, you can find me
+      In the meantime, you can find me
       <a
         class="text-indigo hover:text-indigo-light"
         href="https://www.linkedin.com/in/lucian-eduard-ghimpu-21b33b131/"
