187bab404e60aecd219e9826899e79e1feb0eac1
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2022-09-06 23:41:45 +0300

    fix: removed script

--- src/components/ProjectSummary.vue
+++ src/components/ProjectSummary.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="flex flex-col container">
-    <div>
-        
-    </div>
+    <div></div>
   </div>
 </template>
-
-<script setup></script>
-
-<style lang="postcss" scoped></style>
