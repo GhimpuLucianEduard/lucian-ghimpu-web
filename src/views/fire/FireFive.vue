80a630816c3877553e50aead03b824710431df0e
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2023-12-31 09:47:38 +0200

    fix: title

--- src/views/fire/FireFive.vue
+++ src/views/fire/FireFive.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-start items-center container w-full mt-12 pb-12">
     <article class="prose dark:prose-invert lg:w-[856px]">
-      <h1>I reached $100k - end of the year report</h1>
+      <h1>I Reached $100k - End of the Year Report</h1>
 
       <em>December 31, 2023 - Reading Time: 7 minutes</em>
 
