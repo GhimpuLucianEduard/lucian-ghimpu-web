c61c61c60a38b5c244df92d8d56566ed48486b33
Ghimpu Lucian Eduard <ghimpulucianeduard@gmail.com>
2024-01-05 15:39:37 +0200

    fix: bg color

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bottom-0 flex flex-row text-white w-full pt-8 pb-8 container bg-indigo-light">
+  <div class="bottom-0 flex flex-row text-white w-full pt-8 pb-8 container">
     <div class="flex flex-row ml-auto space-x-4 md:space-x-8">
       <router-link class="text-footer" to="/cookie-policy">Cookie Policy</router-link>
       <router-link class="text-footer" to="/privacy-policy">Privacy Policy</router-link>
